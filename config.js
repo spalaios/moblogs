a7b31978210f7fcf5e9455f2c88561f2d489af13
spalaios <palaisuraj@gmail.com>
2019-06-27 01:38:12 +0530

    base level setup and db connection done

---
+++ config.js
@@ -0,0 +1,6 @@
+const url = Object.seal({
+  db: 'mongodb://localhost:27017/moblogs',
+  app: 3000
+});
+
+module.exports = url;
---
+++ models/Users.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  contact_no: {
+    type: NumberLong
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
